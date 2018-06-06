473b42f7afab094a096d3edf135e92454683bf31
Martha Ayalew <Marti@Marthas-MacBook-Pro.local>
2018-06-05 21:14:57 -0400

    minor update to sign-in page

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+Drops the leaderboard if it exists currently --
+DROP DATABASE IF EXISTS leaderboard;
+-- Creates the "todolist" database --
+CREATE DATABASE leaderboard;
+USE leaderboard;
+CREATE TABLE Scores(
+    name VARCHAR(10) NOT NULL DEFAULT 'Anonymous' PRIMARY KEY,
+    score INT(5) UNSIGNED NOT NULL DEFAULT '0',
+    ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
