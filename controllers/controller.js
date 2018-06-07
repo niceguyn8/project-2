709558b0c6c8a16907e78453a79823362bd0a45b
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2018-06-06 22:18:49 -0400

    fix more

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,7 @@ var express = require("express");
 
 var express = require("express");
 var router = express.Router();
-var quiz = require("../models/quiz.js");
+// var quiz = require("../models/quiz.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
       res.render("createQuiz");
