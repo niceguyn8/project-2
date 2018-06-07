596d7e67b11529a0b93445b4e6cd6c7150244460
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2018-06-06 22:16:12 -0400

    idk

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-// 
-// var express = require("express");
-//
-//
-// var express = require("express");
-// var router = express.Router();
-// var quiz = require("../models/quiz.js");
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//       res.render("createQuiz");
-// });
-//
-// router.post("/Scores/create", function(req, res) {
-//   result.create(req.body.result_)
-//   res.render("")
-//
-//
-// })
-// // Export routes for server.js to use.
-// module.exports = router;
-=======
 
 var express = require("express");
 
@@ -40,4 +18,3 @@ router.post("/Scores/create", function(req, res) {
 
 // Export routes for server.js to use.
 module.exports = router;
->>>>>>> Stashed changes
--- server.js
+++ server.js
@@ -47,15 +47,18 @@ var db = require("./models");
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
-<<<<<<< Updated upstream
 
-// Static directory
 app.use(express.static("public"));
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Routes
-// =============================================================
+var routes = require("./controllers/controller.js");
+app.use(routes);
+
+
 require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -63,21 +66,4 @@ db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-=======
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-var routes = require("./controllers/controller.js");
-app.use(routes);
-
-  require("./routes/api-routes.js")(app);
-  require("./routes/html-routes.js")(app);
-
-  // Syncing our sequelize models and then starting our Express app
-  // =============================================================
-  db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
->>>>>>> Stashed changes
 });
